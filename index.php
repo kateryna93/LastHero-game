c850d2440f5ff6a14d7ff09aa88a056662bb9888
Kateryna Osmachko <kateryna.y@ukr.net>
2016-03-15 10:42:28 +0200

    Create game

--- index.php
+++ index.php
@@ -22,3 +22,27 @@ echo "<strong>List of participants</strong><br>";
 echo "<pre>";
 print_r($participants);
 echo "</pre>";
+
+//Create game
+echo "<br><h1>Game is starting:</h1><br>";
+for($date=1; $date <= 30; $date++) {
+    $rand_keyNumber = array_rand($participants, 1);
+    $rand_participant = $participants[$rand_keyNumber];
+    $participant = " ";
+
+    foreach($rand_participant as $key => $value) {
+        if ($key == 'age'){
+            $participant .= ", ".$value." age";
+        }else{
+            $participant .= " ".$value;
+        }
+    }
+    if($date == 30){
+        echo '<h2>The winner is</h2><h3>' . $participant . '</h3><br>';
+    } else {
+        echo '<b>' . $date . '</b><br>';
+        echo 'out of game: ' . $participant . '<br><br>';
+    }
+
+    unset($participants[$rand_keyNumber]);
+}
